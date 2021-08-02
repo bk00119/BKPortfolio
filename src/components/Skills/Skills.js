605bcf33114e9cbe9888f4845e0c641342546869
Dr. Kim <39502789+bk00119@users.noreply.github.com>
2021-08-02 23:45:31 +0900

    skill spacing

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,17 +8,17 @@ const Skills = () => {
       {/* <SkillSphere /> */}
       <div class="Skill_Description">
         <div class="Skill_Description_P">
-          I started my journey as a Full-Stack Developer in2016, creating web
-          appsfor my High School.
+          I started my journey as a Full-Stack Developer in 2016, creating web
+          apps for my High School.
         </div>
         <br />
         <div className="Skill_Description_P">
-          Then, I learned ComputerScience in depth at NewYork University.
+          Then, I learned Computer Science in depth at New York University.
         </div>
         <br />
         <div class="Skill_Description_P">
-          While I was in Republic ofKorea Army, I spent myfree time creating a
-          webplatform, HostFam.
+          While I was in Republic of Korea Army, I spent my free time creating a
+          web platform, HostFam.
         </div>
       </div>
     </div>
