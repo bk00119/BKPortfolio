5ac15fd7f28f4381a54d8d29a3e7dae9c4682192
Dr. Kim <39502789+bk00119@users.noreply.github.com>
2021-08-07 23:28:42 +0900

    fixed the prev problem

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import SkillSphere from "./SkillSphere";
-import Sphere from "./Sphere";
+// import Sphere from "./Sphere";
 import "./Skills.css";
 
 const Skills = () => {
