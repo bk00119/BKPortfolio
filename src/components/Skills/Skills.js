c37fe66cc841910b1dcb0cea35ff070066aaefca
Dr. Kim <39502789+bk00119@users.noreply.github.com>
2021-08-08 00:01:27 +0900

    sphere size changing based on the device size

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -29,23 +29,25 @@ const Skills = () => {
       <h2 class="Page_Title">My Skills</h2>
       {/* <SkillSphere /> */}
       {/* <Sphere /> */}
-      <div class="SkillSphere">
-        <SkillSphere />
-      </div>
+      <div class="Skills_Detail">
+        <div class="SkillSphere">
+          <SkillSphere />
+        </div>
 
-      <div class="Skill_Description">
-        <div class="Skill_Description_P">
-          I started my journey as a Full-Stack Developer in 2016, creating web
-          apps for my High School.
-        </div>
-        <br />
-        <div className="Skill_Description_P">
-          Then, I learned Computer Science in depth at New York University.
-        </div>
-        <br />
-        <div class="Skill_Description_P">
-          While I was in Republic of Korea Army, I spent my free time creating a
-          web platform, HostFam.
+        <div class="Skill_Description">
+          <div class="Skill_Description_P">
+            I started my journey as a Full-Stack Developer in 2016, creating web
+            apps for my High School.
+          </div>
+          <br />
+          <div className="Skill_Description_P">
+            Then, I learned Computer Science in depth at New York University.
+          </div>
+          <br />
+          <div class="Skill_Description_P">
+            While I was in Republic of Korea Army, I spent my free time creating
+            a web platform, HostFam.
+          </div>
         </div>
       </div>
     </div>
